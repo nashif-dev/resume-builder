973941a2ea4d6a1bcd68f8da42babf724ab99a0f
nashif <nashift04@gmail.com>
2025-10-23 03:52:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from '@mui/material/Button';
+import { FaHome } from "react-icons/fa";
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
